c07a687107c8f12ea385ef179ffbf2ca5ae0d269
Shane-Gray394 <86175936+Shane-Gray394@users.noreply.github.com>
2021-11-15 09:45:05 -0500

    wip

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,5 @@
-require('dotenv').config()
+require("dotenv").config();
 /*
-
-  PORT=9000
-  NODE_ENV=development
-  DEV_DATABASE_URL=postgresql://postgres:password@localhost:5432/database_name
-  TESTING_DATABASE_URL=postgresql://postgres:password@localhost:5432/testing_database_name
 
   Put the above in your .env file. Some adjustments in the connection URLs will be needed:
 
@@ -15,17 +10,17 @@ require('dotenv').config()
     - testing_database_name (use the real name of the testing database you created in pgAdmin 4)
 
 */
-const pg = require('pg')
+const pg = require("pg");
 
 if (process.env.DATABASE_URL) {
-  pg.defaults.ssl = { rejectUnauthorized: false }
+  pg.defaults.ssl = { rejectUnauthorized: false };
 }
 
 const sharedConfig = {
-  client: 'pg',
-  migrations: { directory: './api/data/migrations' },
-  seeds: { directory: './api/data/seeds' },
-}
+  client: "pg",
+  migrations: { directory: "./api/data/migrations" },
+  seeds: { directory: "./api/data/seeds" },
+};
 
 module.exports = {
   development: {
@@ -41,4 +36,4 @@ module.exports = {
     connection: process.env.DATABASE_URL,
     pool: { min: 2, max: 10 },
   },
-}
+};
